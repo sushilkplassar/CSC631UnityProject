2f9664a73603db145280573ded01b74ef6c9f1fa
Sunny Wong <swong6@mail.sfsu.edu>
2019-02-18 18:45:57 -0800

    Finished camera switching, started on working on how to pick up objects

---
+++ Assets/GrabObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+    // Camera will act as like a hand to pick up objects.
+    private Camera hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hand = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When player clicks left button, try to pick up object.
+        if(Input.GetMouseButtonDown(0))
+        {
+            // Point is pixel of what you are trying to pick up. 
+            Vector3 point = new Vector3(hand.pixelWidth / 2, hand.pixelHeight / 2, 0);
+            Ray ray = hand.ScreenPointToRay(point);
+            RaycastHit objectGrabbed;
+            if (Physics.Raycast(ray, out objectGrabbed))
+            {
+                Debug.Log("Object coordinates to be grabbed " + objectGrabbed.point);
+            }
+
+        }
+        
+    }
+}
