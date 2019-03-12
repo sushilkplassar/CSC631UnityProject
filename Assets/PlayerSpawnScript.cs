c2b3802f25cb00264cb7610ed5666642001a58a7
Sunny Wong <swong6@mail.sfsu.edu>
2019-03-12 15:46:26 -0700

    Added way to spawn two players, but unable to update correctly

--- Assets/PlayerSpawnScript.cs
+++ Assets/PlayerSpawnScript.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerSpawnScript : MonoBehaviour
+public class PlayerSpawnScript : NetworkBehaviour
 {
+    public GameObject PlayerObjectPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Checks to see if this is another player that doesn belong to the local player. 
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+        // Instantiate(PlayerObjectPrefab);
+        CmdSpawnPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    [Command]
+    void CmdSpawnPlayer()
+    {
+        GameObject player = Instantiate(PlayerObjectPrefab);
+        NetworkServer.Spawn(player);
     }
 }
