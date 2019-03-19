4532213fe87c83d8739d3e844b400e55fb60277b
awexli <li_alex1415@hotmail.com>
2019-03-19 16:28:26 -0700

    folder organization

---
+++ Assets/__Scripts/MovementScripts/Test_Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Movement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveForwardAndBack = Input.GetAxis("Vertical") * moveSpeed; // forward and backward movements is Z axis
+        float moveSideToSide = Input.GetAxis("Horizontal") * moveSpeed; // Moving side to side is X axis
+    }
+}
