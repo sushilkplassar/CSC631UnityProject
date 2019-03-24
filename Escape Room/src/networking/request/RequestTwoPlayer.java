dd71fe50c70b96bf35628b8683fb57e7f4574845
sushilkplassar <sushilkplassar@gmail.com>
2019-03-23 19:53:19 -0700

    Request Two Player logic modified

--- Escape Room/src/networking/request/RequestTwoPlayer.java
+++ Escape Room/src/networking/request/RequestTwoPlayer.java
@@ -18,7 +18,7 @@ public class RequestTwoPlayer extends GameRequest {
     // Data
     private String version;
     private String user_id;
-    private int count;
+    private static int count=0;
     private String err_response = "You are the only player in the room";
     private String success_response = "Congratulations.. You got the match to play ";
    
@@ -29,7 +29,6 @@ public class RequestTwoPlayer extends GameRequest {
     	responses.add(responseTwoPlayer = new ResponseTwoPlayer());
     }
  
-    
     @Override
     public void parse() throws IOException {
     	System.out.println("I am in RequestTwoPlayer Class");
@@ -54,5 +53,6 @@ public class RequestTwoPlayer extends GameRequest {
         	responseTwoPlayer.setStatus((short)0);
     		responseTwoPlayer.setNumberOfPlayerResponse(success_response);
     	}
+//        count=0;
     }
 }
