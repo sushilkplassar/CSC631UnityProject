a5f652204daa7ec3e73b2c5be17d07e7385d53f4
Sunny Wong <swong6@mail.sfsu.edu>
2019-03-25 01:08:35 -0700

    removed .idea folder

--- escape_room/src/core/GameClient.java
+++ escape_room/src/core/GameClient.java
@@ -113,6 +113,7 @@ public class GameClient implements Runnable {
                     // If there was no activity for the last moments, exit loop
                     if ((System.currentTimeMillis() - lastActivity) / 1000 >= Constants.TIMEOUT_SECONDS) {
                         isDone = true;
+                        
                     }
                 }
             } catch (Exception ex) {
