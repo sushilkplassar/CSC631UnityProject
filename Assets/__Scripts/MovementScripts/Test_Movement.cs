7ce1ad3c82043d2968b3dedd67316eac2791a62c
awexli <li_alex1415@hotmail.com>
2019-04-04 19:40:40 -0700

    updated player 2 puzzle, renamed my scene, added folder for my scripts

--- Assets/__Scripts/MovementScripts/Test_Movement.cs
+++ Assets/__Scripts/MovementScripts/Test_Movement.cs
@@ -18,8 +18,8 @@ public class Test_Movement : MonoBehaviour
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
 
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3 (moveHorizontal, -0.05f, moveVertical);
 
-        rb.AddForce (movement * speed);
+        rb.velocity = movement * speed;
     }
 }
