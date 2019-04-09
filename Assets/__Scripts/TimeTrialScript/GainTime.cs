c6f3d99771b971c534426977af1f10e2ca3241a8
BenLouie <benjaminlouie962@gmail.com>
2019-04-08 22:33:18 -0700

    Timer for the player

---
+++ Assets/__Scripts/TimeTrialScript/GainTime.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class GainTime : MonoBehaviour
+{
+    private TimeControl timerScript;
+
+    void Start()
+    {
+        timerScript = GameObject.Find("PlayerObject").GetComponent<TimeControl>();
+    }
+    void OnTriggerEnter(Collider Col)
+    {
+        if (Col.tag == "Player")
+        {
+            timerScript.timer += 10;
+            timerScript.guiShow = true;
+            Destroy(gameObject);
+        }
+    }
+}
