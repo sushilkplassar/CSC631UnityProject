4f5461e5acbe4616d4c6fb4719fe791969377c7a
awexli <li_alex1415@hotmail.com>
2019-04-15 02:14:43 -0700

    cleaned starting environment, death wall starts when triggered, death wall stops when puzzle is finished.

---
+++ Assets/__Scripts/Alex's Scripts/TriggerDeathWall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDeathWall : MonoBehaviour
+{
+    public Lerp_Tile lerpWall;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Trigger")
+        {   
+            lerpWall.steppedOn = true;
+        }
+    }
+}
