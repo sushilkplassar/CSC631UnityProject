cb606409ba801ac826de8d53247e568afd0cdacf
awexli <li_alex1415@hotmail.com>
2019-04-20 01:38:10 -0700

    scholars sliding door working

--- Assets/__Scripts/Alex's Scripts/FadeToLevel.cs
+++ Assets/__Scripts/Alex's Scripts/FadeToLevel.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class FadeToLevel : MonoBehaviour
 {
+     public CanvasGroup canvasGroup;
+     private float elapsedTime;
+     private float fadeTime = 1f;
+    
+    void Update()
+    {
+        DoFadeOut();
+    }
+
+    IEnumerator DoFadeOut()
+     {
+         while(canvasGroup.alpha > 0)
+         {
+             elapsedTime += Time.deltaTime;
+             canvasGroup.GetComponent<CanvasGroup>().alpha = Mathf.Clamp01(1.0f - (elapsedTime / fadeTime));
+             yield return null;
+         }
+ 
+         yield return null;
+     }
 }
 
 
--- Assets/__Scripts/Alex's Scripts/TriggerDeathWall.cs
+++ Assets/__Scripts/Alex's Scripts/TriggerDeathWall.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class TriggerDeathWall : MonoBehaviour
 {
     public Lerp_Tile lerpWall;
+    public SlideWall[] slideWall;
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.gameObject.tag == "Trigger")
         {   
             lerpWall.steppedOn = true;
+        }
+
+        if (hit.gameObject.tag == "WallSlide")
+        {
+            slideWall[0].tileStepped = true;
+            slideWall[1].tileStepped = true;
+            slideWall[2].tileStepped = true;
         }
     }
 }
