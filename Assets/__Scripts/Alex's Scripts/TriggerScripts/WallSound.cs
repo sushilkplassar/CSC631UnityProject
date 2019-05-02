daaa67328a3352a7063838222960ead74511aadd
awexli <li_alex1415@hotmail.com>
2019-05-01 17:03:49 -0700

    wall sliding sound, attached p1 puzzle2 room

---
+++ Assets/__Scripts/Alex's Scripts/TriggerScripts/WallSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSound : MonoBehaviour
+{
+    public SlideObject_Stay wall;
+    void Update()
+    {
+        if (wall.tileStepped == true && GetComponent<AudioSource>().isPlaying == false)
+        {
+
+            GetComponent<AudioSource>().volume = Random.Range(0.4f, 0.6f);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
