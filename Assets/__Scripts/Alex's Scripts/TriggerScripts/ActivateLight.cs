79850c4b1cf648854beaf0f9d84f9c5076d084c4
awexli <li_alex1415@hotmail.com>
2019-05-05 21:25:20 -0700

    puzzle 2 update - main components working

--- Assets/__Scripts/Alex's Scripts/TriggerScripts/ActivateLight.cs
+++ Assets/__Scripts/Alex's Scripts/TriggerScripts/ActivateLight.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ActivateLight : MonoBehaviour
 {
     public bool inZone = false;
-    public bool ePressed = false;
+    
     public EnableLight[] lights;
     
     void Update()
@@ -14,11 +14,16 @@ public class ActivateLight : MonoBehaviour
         {
             if (Input.GetKeyDown("e"))
             {
-                ePressed = true;
+                // P1 Torch
                 lights[0].myLight.enabled = !lights[0].myLight.enabled;
+                // P1 WallLight
                 lights[1].myLight.enabled = !lights[1].myLight.enabled;
+
+                // P2 LightColors
                 lights[2].myLight.enabled = !lights[2].myLight.enabled;
-                //inZone = false;
+                // P2 WallLight
+                lights[3].myLight.enabled = !lights[3].myLight.enabled;
+
             }
         }
     } 
---
+++ Assets/__Scripts/Alex's Scripts/TriggerScripts/EnableRender.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableRender : MonoBehaviour
+{
+    public Renderer renderObject;
+
+    void Start()
+    {
+        renderObject = GetComponent<Renderer>();
+        //renderObject.enabled = true;
+    }
+}
