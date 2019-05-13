0f0792663e8558d13043ee575959fd7ff077af38
awexli <li_alex1415@hotmail.com>
2019-05-12 22:24:01 -0700

    dontdestroyonload, and misc scripts

---
+++ Assets/__Scripts/Alex's Scripts/Unused/InstantiateRuntime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateRuntime : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        Instantiate(myPrefab, new Vector3(21.95f,0.922f, -19.408f), Quaternion.identity);
+    }
+}
---
+++ Assets/__Scripts/Alex's Scripts/Unused/SwitchPlayers.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPlayers : MonoBehaviour
+{
+    public GameObject playerCam;
+    public GameObject player1Cam;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("2"))
+        {
+            playerCam.SetActive(false);
+            player1Cam.SetActive(true);
+            
+            GameObject.Find("Player 1").GetComponent<ClientSide_Movement>().speed = 0;
+            GameObject.Find("Player 2").GetComponent<ClientSide_Movement>().speed = 3;
+
+            GameObject.Find("Player 1").GetComponent<MouseLook>().enabled = false;
+            GameObject.Find("Player 2").GetComponent<MouseLook>().enabled = true;
+        }
+
+        if(Input.GetKeyDown("1"))
+        {
+            player1Cam.SetActive(false);
+            playerCam.SetActive(true);
+            
+            GameObject.Find("Player 2").GetComponent<ClientSide_Movement>().speed = 0;
+            GameObject.Find("Player 1").GetComponent<ClientSide_Movement>().speed = 3;
+
+            GameObject.Find("Player 1").GetComponent<MouseLook>().enabled = true;
+            GameObject.Find("Player 2").GetComponent<MouseLook>().enabled = false;
+        }
+    }
+}
