c8ad6d2fbf5b20dc777bd28bdb984b25d8af757b
awexli <li_alex1415@hotmail.com>
2019-05-13 01:05:46 -0700

    Player does not use TriggerWall Script anymore, still need to figure out respawn

---
+++ Assets/__Scripts/Alex's Scripts/TriggerScripts/SlideObject_Rev.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideObject_Rev : MonoBehaviour
+{
+    // Transforms to act as start and end markers for the journey.
+    //public Transform originMarker;
+    public Transform endMarker;
+    public GameObject wallPos;
+    
+    public float speed = 1.0F;
+    public bool tileStepped = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tileStepped == true)
+        {
+            StartCoroutine(moveToMarker());
+            if(wallPos.transform.position == endMarker.position)
+            {
+                tileStepped = false;
+            }
+        }
+    }
+
+    IEnumerator moveToMarker()
+    {
+        yield return new WaitForSeconds(0.2f);
+        transform.position = Vector3.MoveTowards(wallPos.transform.position, endMarker.position, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/__Scripts/Alex's Scripts/TriggerScripts/TriggerZoneRev.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneRev : MonoBehaviour
+{
+    // Object that is to be slide/moved
+    public SlideObject_Stay[] slideStay;
+    public SlideObject_Rev[] slideRev;
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //if(slideRev[1].tileStepped = false)
+            //{
+                slideStay[0].tileStepped = true;
+                slideStay[1].tileStepped = true;
+            //}
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        //if(slideStay[1].tileStepped = false)
+        //{
+            slideRev[0].tileStepped = true;
+            slideRev[1].tileStepped = true;
+        //}
+    }
+}
