295bb88566fea44d5aaad2bf307cea595eb47143
Benjamin Louie <blouie1@mail.sfsu.edu>
2019-05-15 15:40:12 -0700

    final maze finishes

---
+++ Assets/__Scripts/Alex's Scripts/FinalScripts/CloseGateBehind.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGateBehind : MonoBehaviour
+{
+
+    public SlideObject_Rev slideRev;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            slideRev.tileStepped = true;
+        }
+    }
+}
